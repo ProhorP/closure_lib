5f684833f2e73b87bd88e9b3168de628016059b3
Petr Usoltsev <usoltsev05@mail.ru>
2023-02-17 13:34:06 +0800

    test memcpy func

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ const byte src_pattern[] = { 0xf3, 0x0f, 0x1e, 0xfa,
 
 typedef int (*closure_t) (int);
 typedef int (*ptr_func) (int);
-typedef void (*test_print) (void);
+typedef int (*test_print) (int);
 
 closure_t closure_save_func;
 int closure_save_val;
@@ -49,6 +49,26 @@ test_func (int a)
   return a + closure_save_val;
 }
 
+int
+hello1 (int a, int b)
+{
+
+  return a + b;
+
+}
+
+int
+closure_pattern (int val)
+{
+  return hello1 (6, val);
+}
+
+int
+closure_pattern_1 (int val)
+{
+  return hello1 (7, val);
+}
+
 test_print
 create_func (int number)
 {
@@ -62,8 +82,16 @@ create_func (int number)
     print_error ("%s\n",
 		 "Ошибка вызова функции mmap для входного файла");
 
-  memcpy (src, src_pattern, sizeof (src_pattern));
-//snprintf (src, src_len, src_pattern, number);
+  int func_len = 0;
+  byte *ptr_pattern = (byte *) closure_pattern;
+  for (func_len = 0; ptr_pattern[func_len] != 0xc3;)
+    func_len++;
+
+
+  func_len++;
+
+
+  memcpy (src, ptr_pattern, func_len);
 
   return (test_print) src;
   //uint32_t a = crc32 ((const void *) src, statbuf.st_size);
@@ -72,21 +100,6 @@ create_func (int number)
 
 }
 
-int
-closure_func (int val)
-{
-/*вот тут не могу понять как сопоставить эту функцию
-с данными, которые передавались в closure_make
-МОжно ли создавать не указатель на closure_func, а
-указатель на данные и этот указатель передавать как
-первый параметр в процедуру например EXEC_CLOSURE:
-EXEC_CLOSURE(ptr_closure, ...)
-Переменное число аргументов будут перенапрявляться
-в нужную функцию
-*/
-  return closure_save_func (val);
-
-}
 
 closure_t
 closure_make (ptr_func func, int val)
@@ -95,7 +108,7 @@ closure_make (ptr_func func, int val)
   closure_save_val = val;
   closure_save_func = func;
 
-  return closure_func;
+  return closure_pattern;
 
 }
 
@@ -107,24 +120,21 @@ delete_closure (closure_t adder)
 
 }
 
-void
-hello ()
-{
-
-  //printf ("%d", 9);
-
-}
 
 int
 main ()
 {
 
-  hello ();
+  int n = 0;
 
-  int n = 3;
+  n = closure_pattern (1);
+  printf ("%d\n", n);
 
   test_print adder = create_func (n);
-  adder ();
+
+  n = adder (3);
+  printf ("%d\n", n);
+
 //  closure_t adder = closure_make(test_func, n);
 
   //printf ("%d\n", adder (2)); // ожидаем увидеть 5
